3a6b2633482fc6b4fb433c23349ee22b52a7089a
Alexander Dmitriev <test@example.com>
2020-10-05 21:24:58 +0400

    Vars and arrays learning

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-let name = 'John'
-name = 4
-name = true
+// var name2 = "John 'Smith'"
+// let name = 'Ivan "Drago"'
+// name = 'Elena'
+// const name3 = 'Jane'
+// const hello = "Привет"
+// console.log('hello ' + name)
+// console.log('hello ' + name2)
+// console.log(`${hello} ${name3}!!!!!!!`)
 
-// var varId
-let hello = 'Hello ' + name
-console.log(hello)
+const hello = ['Hello', 'Привет']
+const array = ['John', 'Ivan', 'Jane']
+
+for (let helloIndex = 0; helloIndex < hello.length; helloIndex++) {
+    for (let index = 0; index < array.length; index++) {
+        console.log(hello[helloIndex] + ' ' + array[index])
+    }
+}
+
